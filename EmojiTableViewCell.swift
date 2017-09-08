4dec355a12a3aa2b63d1214dc33aa5f7f1b5b258
Saya Murton <saya.murton@gmail.com>
2017-09-08 13:46:33 -0600

    New Cocoa Touch Class file
    -added new Cocoa Touch Class file named "EmojiTableViewCell" (subclass UITableViewCell)
    -updated the identy of the Emoji Cell to be an EmojiTableViewCell

---
+++ EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by Saya Murton on 9/8/17.
+//  Copyright © 2017 Saya Murton. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
